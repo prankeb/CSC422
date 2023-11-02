38cdb7b783da5c75faf48e0156a46ba1c784257b
Brandon Pranke <benon@DESKTOP-M7PN91A>
2023-11-02 16:55:07 -0500

    Third version

--- PrankeDatabase/src/prankedatabase/PrankeDatabase.java
+++ PrankeDatabase/src/prankedatabase/PrankeDatabase.java
@@ -26,6 +26,7 @@ public class PrankeDatabase {
         
         //Calls the main menu method to display the main menu
         mainMenu(pets);
+        System.out.println("GoodBye!");
        
     }
     
@@ -33,6 +34,8 @@ public class PrankeDatabase {
     
     //Method for displaying the main menu
     public static void mainMenu(ArrayList<Pet> pets){
+        int choice = 0;
+        while (choice !=7){
         Scanner input = new Scanner(System.in);
         System.out.println("\nWhat would you like to do\n1) View all pets\n2) Add more pets");
 	System.out.println("3) Update an existing pet\n4) Remove an existing pet");
@@ -40,16 +43,19 @@ public class PrankeDatabase {
 	System.out.print("Your choice: ");
         
         //Grabs the user selection of the main menu
-        int choice = input.nextInt();
+         choice = input.nextInt();
         
         //Calls a different method depending on the choice the made
         switch(choice){
             case 1: showAllpets(pets);break;
             case 2: addPets(pets);break;
+            case 3: updatePet(pets);break;
+            case 4: removePet(pets);break;
             case 5: searchPetName(pets);break;
             case 6: searchPetAge(pets);break;
             default: break;
         }
+    }
     }
     
     //Method for adding pets to the array list
@@ -74,7 +80,6 @@ public class PrankeDatabase {
             else{
                 //Shows users how many pets have been added 
                 System.out.println(petCounter + " pets added");
-                mainMenu(pets);
             }
         }
     }
@@ -99,12 +104,14 @@ public class PrankeDatabase {
 	System.out.println("\n+-------------------------+\n" + rowCount + " rows in set");
 	}
     
+    //Method allows user to search pet by age
     public static void searchPetAge(ArrayList<Pet> pets){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter age to search: ");
         int petAge = input.nextInt();
         printTableHeader();
         int petCounter = 0;
+        //Loops through each pet and compares it to the name the user put in
         for(int i = 0; i < pets.size(); i++){
             Pet pet = pets.get(i);
             int age = pet.getAge();
@@ -115,26 +122,50 @@ public class PrankeDatabase {
             }
         }
         printTableFooter(petCounter);
-        mainMenu(pets);
     }
     
+    //Method allowing User to search for pet by name
     public static void searchPetName(ArrayList<Pet>pets) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a name to search: ");
-        String petName = input.next();
+        String petName = input.next().toLowerCase();
         printTableHeader();
         int petCounter = 0;
         for(int i = 0; i < pets.size(); i++){
             Pet pet = pets.get(i);
             String name = pet.getName();
+            String lowerName = name.toLowerCase();
             int age = pet.getAge();
-            if(petName.equals(name)){
+            if(petName.equals(lowerName)){
                 printTableRow(i, name, age);
                 petCounter++;
             }
         }
         printTableFooter(petCounter);
-        mainMenu(pets);
+    }
+    
+    //Method for removing Pet
+    public static void removePet(ArrayList<Pet> pet){
+        //Displays all the pets
+        Scanner input = new Scanner(System.in);
+        showAllpets(pet);
+        
+        //Gets the id for the removed pet from user
+        System.out.print("Enter the pet Id to remove: ");
+        int id = input.nextInt();
+        
+        //Gets the removed pet name and age
+        Pet noPet = pet.get(id);
+        String name = noPet.getName();
+        int age = noPet.getAge();
+        
+        //Displays message
+        System.out.print(name + " " + age + " is removed");
+        
+        //Removes pet and brings user to main menu
+        pet.remove(id);
+        
+        
     }
     //Method for displaying all pets in the array list
     public static void showAllpets(ArrayList<Pet> pets){
@@ -154,9 +185,34 @@ public class PrankeDatabase {
         
         //Calls method to print table footer
         printTableFooter(pets.size());
+    }
+    
+    public static void updatePet(ArrayList<Pet> pet){
+        //Displays all the pets
+        Scanner input = new Scanner(System.in);
+        showAllpets(pet);
         
-        //Takes the user back to the main menu
-        mainMenu(pets);
+        //Gets the id of the pet to update
+        System.out.print("Enter the pet ID you wish to update: ");
+        int id = input.nextInt();
+        
+        //Gets the new name and age
+        System.out.print("Enter new name and new age: ");
+        String name = input.next();
+        int age = input.nextInt();
+        
+        //Gets the orginal pet info
+        Pet oldPet = pet.get(id);
+        String oldName = oldPet.getName();
+        int oldAge = oldPet.getAge();
+        
+        //Displays message
+        System.out.println(oldName + " " + oldAge + " changed to " + name + " " + age);
+        
+        //Updates the pet
+        oldPet.setAge(age);
+        oldPet.setName(name);
+        
     }
 
     
